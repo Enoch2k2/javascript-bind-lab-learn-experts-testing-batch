e283276a86992335fa31d781a6742829d7d5e8fc
Enoch2k2 <enoch2k2@hotmail.com>
2016-10-04 08:19:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+var digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function(){
+    setInterval(() => this.time++, 1000)
+  }
+};
+
+function censor(word, string){
+  var censored = new RegExp(word, 'gi');
+  return string.replace(censored, 'BLEEP');
+}
+
+var violenceCensor = censor.bind(null, 'violence');
+var drugsCensor = censor.bind(null, 'drugs');
